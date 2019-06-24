8242cab082d11042a8d6ac2d5558404430e63503
chenjingjing <1261005259@qq.comm>
2019-06-24 20:37:40 +0800

    'admin名字、登出实现’

--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -1,16 +1,51 @@
+/**
+ * 主页头部主组件：HeaderMain
+ * 非路由组件
+ */
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import MyButton from '../my-button';
 import logo from '../../assets/image/qing.png';
+import {getItem, removeItem} from '../../utils/storage-tools';
+import { Modal, Button } from 'antd';
+
+
+
 import './index.less';
+const { confirm } = Modal;
 
 class HeaderMain extends Component {
+
+
+    componentWillMount() {
+        //读取用户名
+        this.username = getItem().username;
+    }
+
+    logout = () => {
+        confirm({
+            title: '您确认要退出登录吗？',
+            okText: '确认',
+            cancelText: '取消',
+            //踩坑：这里要修改成箭头函数，否则里面的this指向会有问题
+            onOk: () => {
+                // 1、先清除本地缓存数据
+                removeItem();
+                // 2、退出登录：Cannot read property 'props' of undefined
+                // HeaderMain不是路由组件 需要通过withRouter进行包装
+                this.props.history.replace('/login');
+            }
+        });
+    };
+
+
 
     render(){
 
         return <div>
             <div className="header-main-top">
-                <span>欢迎，admin</span>
-                <MyButton>退出</MyButton>
+                <span>欢迎，{this.username}</span>
+                <MyButton onClick={this.logout}>退出</MyButton>
             </div>
             <div className="header-main-bottom">
                 <span className="header-main-left">用户管理</span>
@@ -24,4 +59,4 @@ class HeaderMain extends Component {
     }
 }
 
-export default HeaderMain;
+export default withRouter(HeaderMain);
